5ee1edfbaaef770d68243bc0adb43a61d00e75d1
Beth Hart <errordrivendev@gmail.com>
2018-07-06 13:56:24 -0700

    Add project files.

---
+++ GraphDataTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GraphDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
